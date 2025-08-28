419f4bfd490750b73a3dc6c06d9c186b353f8f5c
Juliet-Stewart <juliet.stewart@education.nsw.gov.au>
2025-08-29 08:51:44 +1000

    Code.
    I had previously been working on this in vs code on my laptop. I use linux and haven't been able to install github desktop which has been... annoying. Yes, this code is last minute. I KNOW it's bad. But it's something as opposed to nothing.

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hello World!")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load CSV file
+climate_data = pd.read_csv("climate_data.csv")
+
+#Give the user options
+while True:
+    print("\n=== Climate Data Viewer ===")
+    print("1. View full dataset")
+    print("2. View temperature graph")
+    print("3. Exit")
+
+    choice = input("Choose an option (1–3): ")
+
+    if choice == "1":
+        print(climate_data)
+
+    elif choice == "2":
+        plt.plot(aus_data["Year"], aus_data["Australia Avg Temp (°C)"], label="Australia")
+        plt.plot(global_data["Year"], global_data["Global Avg Temp (°C)"], label="Global")
+        plt.xlabel("Year")
+        plt.ylabel("Temperature (°C)")
+        plt.title("Australia vs Global Temperatures")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Please enter 1, 2, or 3.")
